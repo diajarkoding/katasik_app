e4f5391c0a74e271c89348f11c652ae64f77103c
diajarkoding <diajarkoding@gmail.com>
2022-05-01 06:43:27 +0700

    slicing ui distnace page

--- lib/view/pages/distance_view.dart
+++ lib/view/pages/distance_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:katasik_app/view/widgets/destination_card.dart';
 
+import '../../helper/constans/theme.dart';
 import '../widgets/custom_bottom_navbar_item.dart';
 
 class DistanceView extends StatelessWidget {
@@ -7,9 +9,146 @@ class DistanceView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget address() {
+      return Padding(
+        padding: const EdgeInsets.fromLTRB(
+          defaultMargin,
+          secondaryMargin,
+          defaultMargin,
+          0.0,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Lokasi Anda',
+              style: blackTextStyle.copyWith(
+                fontSize: 24,
+                fontWeight: semiBold,
+              ),
+            ),
+            const SizedBox(
+              height: 6,
+            ),
+            Row(
+              children: [
+                const Icon(
+                  Icons.location_on,
+                  color: greenColor,
+                  size: 20,
+                ),
+                const SizedBox(
+                  width: 3,
+                ),
+                Expanded(
+                  child: Text(
+                    'Madewangi Setiamulya tamansari tasikmalaya jawa barat',
+                    style: greenTextStyle.copyWith(fontSize: 12),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget search() {
+      return Container(
+        width: double.infinity,
+        height: 54,
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        margin: const EdgeInsets.symmetric(
+          horizontal: defaultMargin,
+          vertical: defaultMargin,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100),
+          border: Border.all(
+            color: greyColor,
+            width: 1,
+          ),
+        ),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                right: 16,
+              ),
+              child: Image.asset(
+                'assets/icon_search.png',
+                width: 26,
+              ),
+            ),
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration.collapsed(
+                  hintText: 'Cari Wisata',
+                  hintStyle: greyTextStyle.copyWith(fontWeight: medium),
+                ),
+              ),
+            ),
+            const Icon(
+              Icons.location_on,
+              color: greyColor,
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget destinations() {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              left: defaultMargin,
+              bottom: defaultMargin,
+            ),
+            child: Text(
+              'Wisata Terdekat',
+              style: blackTextStyle.copyWith(
+                fontSize: 20,
+                fontWeight: semiBold,
+              ),
+            ),
+          ),
+          GridView(
+            padding: const EdgeInsets.symmetric(
+              horizontal: defaultMargin,
+            ),
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 0.5,
+              crossAxisSpacing: 18,
+            ),
+            children: [
+              DestinationCard(),
+              DestinationCard(),
+              DestinationCard(),
+              DestinationCard(),
+            ],
+          )
+        ],
+      );
+    }
+
     return Scaffold(
-      body: const Center(
-        child: Text('Distance'),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              address(),
+              search(),
+              destinations(),
+            ],
+          ),
+        ),
       ),
       bottomNavigationBar: CustomBottomNavBarItem(),
     );
---
+++ lib/view/widgets/destination_card.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import '../../helper/constans/theme.dart';
+
+class DestinationCard extends StatelessWidget {
+  const DestinationCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          // width: 150,
+          height: 130,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            image: const DecorationImage(
+              image: NetworkImage(
+                  'https://lh5.googleusercontent.com/p/AF1QipNXQdCCsvauH2TJsu5gzwGDEp7c6U20Dbnoakp2=w529-h298-k-no'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(
+            top: 10,
+            bottom: 5,
+          ),
+          child: Text(
+            'Jarak 40 Km',
+            style: greenTextStyle.copyWith(
+              fontWeight: semiBold,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(bottom: 5),
+          child: Text(
+            'Curug Panetean',
+            style: blackTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: semiBold,
+            ),
+          ),
+        ),
+        Text(
+          'Pangliaran, Pancatengah, Kab. Tasikmalaya',
+          style: blackTextStyle.copyWith(
+            fontSize: 12,
+            fontWeight: reguler,
+          ),
+        ),
+      ],
+    );
+  }
+}
